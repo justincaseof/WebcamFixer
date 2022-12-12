3a333853113dcaa0885782e49491297e4634440d
tone <tobias.nebel@t-systems.com>
2022-12-12 11:28:50 +0100

    Autofocus for Logitech HD5000 Webcam

--- WebcamFixer.cs
+++ WebcamFixer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using AForge.Video.DirectShow;
 
 namespace WebcamFixer
@@ -13,7 +14,20 @@ namespace WebcamFixer
                 VideoCaptureDevice tmp = new VideoCaptureDevice(filterInfo.MonikerString);
                 try
                 {
-                    tmp.SetCameraProperty((CameraControlProperty)(19), 0, CameraControlFlags.None);
+                    if (filterInfo.Name == "Microsoft® LifeCam HD-5000") {
+                        int focus = 0;
+                        CameraControlFlags controlFlags;
+                        tmp.GetCameraProperty(CameraControlProperty.Focus, out focus, out controlFlags);
+
+                        Console.WriteLine("WebCam Name: " + filterInfo.Name + " (" + filterInfo.MonikerString + ")");
+                        Console.WriteLine("\tfocus: " + focus);
+                        Console.WriteLine("\tcontrolFlags: " + controlFlags);
+                        
+                        // leave focus as is, but disable autofocus
+                        Console.WriteLine("--> disabling Autofocus...");
+                        tmp.SetCameraProperty(CameraControlProperty.Focus, focus, CameraControlFlags.Manual);
+                        Console.WriteLine("--> ...done.");
+                    }
                 }
                 catch { }
             }
